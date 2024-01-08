19c4073862b7575f71d5199ba86285d10f87e70f
Ransingh Satyajit Ray <satyajit239@gmail.com>
2024-01-08 20:20:09 +0530

    msg

--- code/D1.py
+++ code/D1.py
@@ -31,7 +31,8 @@ b1 = [5,6,7,8,9]
 print(type(a1.append(b1)))
 print(a1)
 
-# such metods perform their effect by altering the list in-place, not by building and returning a new one.
+# such meth
+# ods perform their effect by altering the list in-place, not by building and returning a new one.
 a1.insert(2,b1)
 print(a1)
 
@@ -43,3 +44,32 @@ print(a1)
 a1.extend(b1)
 print(a1)
 
+# dictionary : get(), values(), keys(), items(), pop(), popitem(), clear(), update(), copy()
+
+dict_1 =   {"key1": [1,2,3,4,5], "key2":[9,7,5,4,3]}
+print(dict_1)
+
+print(dict_1.get("key2")) # key value
+print(dict_1.keys())
+print(dict_1.items())
+print(dict_1.pop("key1"))# removes the key value pair
+print(dict_1)
+
+dict_2 =   {"key1": [1,2,3,4,5], "key2":[9,7,5,4,3]}
+print(dict_2.popitem())
+print(dict_2)
+
+
+nom = int(input("Enter how many nultiplication tables you want:"))
+if(nom<=0):
+    print("{}invalid input".format(nom))
+else:
+    for n in range(1,nom+1):
+        print("________________________")
+        print("\t Mul table for:{}".format(n))
+        print("________________________")
+        for i in range(1,11):
+            print("\t{}X{}={}".format(n,i,n*i))
+        else:
+            print("_____________________")    
+            
